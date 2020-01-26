30a5c3fcf60f3accf240a4014f8badf9275568ef
Dirkadin <dirkadin@gmail.com>
2020-01-26 13:29:59 -0600

    shortened filename

---
+++ room-reservation-service/src/test/java/com/derekriddle/lil/roomreservationservice/RoomReservationServiceAppTests.java
@@ -0,0 +1,13 @@
+package com.derekriddle.lil.roomreservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomReservationServiceAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
