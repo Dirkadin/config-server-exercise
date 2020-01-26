b2d8d2f3a518f139456d98740a8742f7d6d68815
Dirkadin <dirkadin@gmail.com>
2020-01-26 13:28:25 -0600

    initial commit

---
+++ reservation-services/src/main/java/com/derekriddle/lil/reservationservices/ReservationRepository.java
@@ -0,0 +1,11 @@
+package com.derekriddle.lil.reservationservices;
+
+import java.sql.Date;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    Iterable<Reservation> findAllByDate(Date date);
+}
